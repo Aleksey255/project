954455a382772fc8da4577f1f0cb7180433488c4
Alex Druzhinin <aleksey-255@mail.ru>
2024-07-28 12:09:02 +0500

    Added lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = '';
+const screens = '';
+const screenPrice = '';
+const rollback = '';
+const fullPrice = '';
+const adaptive = '';
+
+alert ('Hello !')
+
+console.log('Hello world');

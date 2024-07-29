68b016d137eff4eb2a07c1b138aac709fbc9456b
Alex Druzhinin <aleksey-255@mail.ru>
2024-07-29 22:42:55 +0500

    Added lesson02

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const title = '';
-const screens = '';
-const screenPrice = '';
-const rollback = '';
-const fullPrice = '';
-const adaptive = '';
+const title = 'Project';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 50;
+const rollback = 10;
+const fullPrice = 5000;
+const adaptive = true;
 
-alert ('Hello !')
+// alert ('Hello !')
 
-console.log('Hello world');
+// console.log('Hello world');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей');
+console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log((fullPrice * (rollback/100)));
